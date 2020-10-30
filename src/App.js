5c2b2e9293709ec0d671838fa866942b4fd8537d
Andrius <andrius.rochalazarino@gmail.com>
2020-10-30 09:32:44 -0400

    personagens vindo da api corretamente e tambem em ordem alfabetica

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
 import Character from './components/Character';
+import axios from "axios";
 
 function App() {
+  const getCharacters = async () => {
+    try {
+      const response = await axios.get(`https://swapi.dev/api/people/`);
+      setCharacters(
+        response.data.results.sort((character1,character2)=>{
+          return character1.name[0] > character2.name[0]
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  
+  const [characters, setCharacters] = useState(undefined);
+  console.log(characters)
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
   return (
     <div className="App">
-      <Character eyeColor="blue" name="Luke" />
+      <Character eyeColor="green" name="Luke" />
     </div>
   );
 }
